25e19d564dd0cd8d5d0de2a3666137a9bd4215d5
Anton Sholomitskii <fskunk@yandex.ru>
2023-03-09 20:24:50 +0300

    initial_commit

---
+++ music_base.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS Genre (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS Artist (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL  
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL , 
+    year INTEGER NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS Track (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL ,
+    duration TIME NOT NULL ,
+    album_id INTEGER NOT NULL REFERENCES Album(id)
+);
+
+CREATE TABLE IF NOT EXISTS Compillation (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL ,
+    year INTEGER NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS Genre_artist (
+    id SERIAL PRIMARY KEY,
+    genre_id INTEGER NOT NULL REFERENCES Artist(id),
+    artist_id INTEGER NOT NULL REFERENCES Genre(id)
+);
+
+CREATE TABLE IF NOT EXISTS Artist_album (
+    id SERIAL PRIMARY KEY, 
+    artist_id INTEGER REFERENCES Album(id),
+    album_id INTEGER REFERENCES Artist(id)
+);
+
+CREATE TABLE IF NOT EXISTS Compillation_track (
+    id SERIAL PRIMARY KEY,
+    compillation_id INTEGER REFERENCES Track(id),
+    track_id INTEGER REFERENCES Compillation(id)
+)
